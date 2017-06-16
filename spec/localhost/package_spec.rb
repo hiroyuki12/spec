f6d41fcd36afebedb7f0d8c77ba27f40cba44f3a
hiroyuki12 <hiroyuki12@gmail.com>
2017-06-16 13:41:53 +0900

    update recipe.yml, package_spec.rb, user_spec.rb

--- spec/localhost/package_spec.rb
+++ spec/localhost/package_spec.rb
@@ -1,16 +1,9 @@
 
-describe package('peco') do
-  it { should be_installed }
-end
-describe package('ansible') do
-  it { should be_installed }
-end
-describe package('wget') do
-  it { should be_installed }
-end
-
 # gem
 describe package('serverkit') do
   it { should be_installed.by('gem').with_version('0.6.9') }
 end
 
+describe package('serverspec') do
+  it { should be_installed.by('gem') }
+end
--- spec/localhost/user_spec.rb
+++ spec/localhost/user_spec.rb
@@ -3,18 +3,22 @@ describe user('hiroyuki') do
   it { should exist }
 end
 
-describe command('ls -al /') do
-  its(:stdout) { should match /bin/ }
-end
-
 
 
 describe command('ruby -v') do
   its(:stdout) { should match /2.4.1/ }
 end
 
+describe command('bundler -v') do
+  its(:stdout) { should match /1.15.1/ }
+end
+
 describe command('git --version') do
   its(:stdout) { should match /2.11.0/ }
+end
+
+describe command('vim --version') do
+  its(:stdout) { should match /7.4/ }
 end
 
 
@@ -33,12 +37,18 @@ end
 
 
 
-describe "Command 'ruby -v'" do
+describe "ruby version, gem package'" do
   it "should match /2.4.1/" do
     expect(check_ruby_version).to include("2.4.1")
+  end
+  it "should be_install serverspec" do
+    expect(check_packages("serverspec")).to include("serverspec")
   end
 
   def check_ruby_version
     command("ruby -v").stdout
+  end
+  def check_packages(package)
+    command("bundle exec gem list | grep #{package}").stdout
   end
 end
