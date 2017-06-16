5d51d3aa6fccc63ec43fe0bd4c1a655be7f93b56
hiroyuki12 <hiroyuki12@gmail.com>
2017-06-16 12:27:20 +0900

    update init.sh. add git checkout. delete sample_spec.rb

--- init.sh
+++ init.sh
@@ -4,3 +4,8 @@ bundle init
 echo 'gem "serverspec"' >> Gemfile
 sudo bundle install
 bundle exec serverspec-init
+
+git checkout recipe.yml spec/spec_helper.rb
+cd spec/localhost
+git checkout homebrew_spec.rb myhome_spec.rb package_spec.rb user_spec.rb
+mv sample_spec.rb sample_spec
