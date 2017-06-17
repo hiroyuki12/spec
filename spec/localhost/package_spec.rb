be67a6ca5e3667c5aeb8e80e72e514e54e54073a
hiroyuki12 <hiroyuki12@gmail.com>
2017-06-17 23:39:36 +0900

    delete package_spec.rb.

--- spec/localhost/package_spec.rb
+++
@@ -1,9 +0,0 @@
-
-# gem
-describe package('serverkit') do
-  it { should be_installed.by('gem').with_version('0.6.9') }
-end
-
-describe package('serverspec') do
-  it { should be_installed.by('gem') }
-end
